08f630dabf5d14e6226cc1ffc6c8399942be9ffc
Bader Almatouq <baderalmatouq@gmail.com>
2019-12-10 14:58:39 -0800

    Updated PWM code to generate correct duty cycles

--- PWM_code/PWM_code.ino
+++ PWM_code/PWM_code.ino
@@ -1,29 +1,28 @@
-int pwm_A = 5;
-int pwm_B = 6; 
-int ain_1 = A6;
-int ain_2 = A7;
-int output_A;
-int output_B;
-int pwm_A_value;
-int pwm_B_value;
+int pwm_A = 5;    //Pin associated with first pwm output
+int pwm_B = 6;    //Pin associated with second pwm output
+int ain_1 = A6;   //Pin associated with analog input for first pwm
+int ain_2 = A7;   //Pin associated with analog input for second pwm
+int output_A;     //Variable to store reading from potentiometer for first pwm
+int output_B;     //Variable to store reading from potentiometer for second pwm
+int pwm_A_value;  //Variable to store value to write to first pwm pin
+int pwm_B_value;  //Variable to store value to write to second pwm pin
 void setup() {
+  //Setting associated pins to be outputs
   pinMode(pwm_A, OUTPUT);
-  pinMode(pwm_B, OUTPUT);
+  pinMode(pwm_B, OUTPUT);   
 }
 void loop() {
-  //Reading from potentiometer
+  //Read in values from potentiometer
   output_A = analogRead(ain_1);
-  //Mapping the Values between 0 to 255 because we can give output
-  //from 0 -255 using the analogwrite funtion
-  pwm_A_value = map(output_A, 0, 1023, 0, 255);
-  analogWrite(pwm_A, pwm_A_value);
+  //Map read values between 0 and 38 to generate intended duty cycle values
+  pwm_A_value = map(output_A, 0, 1023, 0, 38);
+  analogWrite(pwm_A, pwm_A_value);   //Write out output to first pwm output pin
   delay(1);
 
-   //Reading from potentiometer
+  //Read in values from potentiometer
   output_B = analogRead(ain_2);
-  //Mapping the Values between 0 to 255 because we can give output
-  //from 0 -255 using the analogwrite funtion
-  pwm_B_value = map(output_B, 0, 1023, 0, 255);
-  analogWrite(pwm_B, pwm_B_value);
+  //Map read values between 0 and 38 to generate intended duty cycle values
+  pwm_B_value = map(output_B, 0, 1023, 0, 38);
+  analogWrite(pwm_B, pwm_B_value);  //Write out output to second pwm output pin
   delay(1);
 }
